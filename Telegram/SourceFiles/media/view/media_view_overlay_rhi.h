9c62ed3eda97421c027dbeb2fe94a1cb7704d767
23rd <23rd@vivaldi.net>
2026-03-31 18:36:46 +0300

    [qrhi] Added stories sibling parts with persistent textures.

--- Telegram/SourceFiles/media/view/media_view_overlay_rhi.h
+++ Telegram/SourceFiles/media/view/media_view_overlay_rhi.h
@@ -175,6 +175,11 @@ private:
 	QSize _controlsAtlasSize;
 	std::array<QRect, kControlsCount + 1> _controlsTextures;
 
+	static constexpr auto kStoriesSiblingPartsCount = 4;
+	QRhiTexture *_storiesSiblingTextures[kStoriesSiblingPartsCount] = {};
+	QSize _storiesSiblingSizes[kStoriesSiblingPartsCount];
+	quint64 _storiesSiblingCacheKeys[kStoriesSiblingPartsCount] = {};
+
 	QRhiTexture *_controlsFadeTexture = nullptr;
 	QSize _controlsFadeSize;
 	bool _shadowTopFlip = false;
