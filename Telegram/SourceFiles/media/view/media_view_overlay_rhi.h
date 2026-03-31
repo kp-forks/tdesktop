48457725c9ed41d0a27e47b69008b2a4f1b8e002
23rd <23rd@vivaldi.net>
2026-03-31 18:35:39 +0300

    [qrhi] Added control icon atlas with over/inner opacity.

--- Telegram/SourceFiles/media/view/media_view_overlay_rhi.h
+++ Telegram/SourceFiles/media/view/media_view_overlay_rhi.h
@@ -82,8 +82,16 @@ private:
 		const QImage &image,
 		ContentGeometry geometry);
 
+	struct Control {
+		int index = -1;
+		not_null<const style::icon*> icon;
+	};
+
 	void createPipelines();
 	void validateControlsFade();
+	void validateControls();
+	void invalidateControls();
+	[[nodiscard]] Control controlMeta(Over control) const;
 
 	void drawTexturedQuad(
 		QRhiGraphicsPipeline *pipeline,
@@ -161,6 +169,11 @@ private:
 	std::vector<PoolTexture> _texturePool;
 	int _nextPoolIndex = 0;
 	[[nodiscard]] QRhiTexture *acquirePoolTexture(QSize size);
+
+	static constexpr auto kControlsCount = 7;
+	QRhiTexture *_controlsAtlasTexture = nullptr;
+	QSize _controlsAtlasSize;
+	std::array<QRect, kControlsCount + 1> _controlsTextures;
 
 	QRhiTexture *_controlsFadeTexture = nullptr;
 	QSize _controlsFadeSize;
