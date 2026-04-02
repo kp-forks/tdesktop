ec3a8be4c0fe2e116eb5a1ba62280309aece4b6c
23rd <23rd@vivaldi.net>
2026-04-02 18:06:13 +0300

    [thanos] Improved collapse animation with position-aware gap.

--- Telegram/SourceFiles/history/history_widget.h
+++ Telegram/SourceFiles/history/history_widget.h
@@ -336,7 +336,7 @@ protected:
 	void mouseMoveEvent(QMouseEvent *e) override;
 
 public:
-	void startCollapseAnimation(int height);
+	void startCollapseAnimation(int height, int itemTop);
 
 private:
 	using TabbedPanel = ChatHelpers::TabbedPanel;
@@ -936,6 +936,7 @@ private:
 	};
 	std::optional<CollapseAnimation> _collapseAnimation;
 	int _collapseHeight = 0;
+	int _collapseItemTop = -1;
 	void collapseAnimationCallback();
 
 	bool _sponsoredMessagesStateKnown = false;
