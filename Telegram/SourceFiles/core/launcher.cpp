f07044f42e0d1a16eb2a0993121054f833c95e29
23rd <23rd@vivaldi.net>
2026-04-04 13:04:38 +0300

    [qrhi] Added Metal availability check with OpenGL fallback for backend.

--- Telegram/SourceFiles/core/launcher.cpp
+++ Telegram/SourceFiles/core/launcher.cpp
@@ -380,7 +380,8 @@ void Launcher::initHighDpi() {
 	if (!OptionUseOpenGLRenderer.value()) {
 		qputenv("QT_WIDGETS_RHI", "1");
 #ifdef Q_OS_MAC
-		qputenv("QT_WIDGETS_RHI_BACKEND", "metal");
+		qputenv("QT_WIDGETS_RHI_BACKEND",
+			Platform::MetalSupported() ? "metal" : "opengl");
 #elif defined(Q_OS_WIN)
 		qputenv("QT_WIDGETS_RHI_BACKEND", "d3d11");
 #else
