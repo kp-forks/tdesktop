99b00a9627f1b269728e7dcb6bdf09bd2622d7bb
23rd <23rd@vivaldi.net>
2026-07-20 20:30:55 +0300

    Updated Qt from Qt 6.10 to 6.11.1 on Windows.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1528,6 +1528,15 @@ release:
     cmake --build out --config Release
 """)
 
+    stage('qt6windows7', """
+win:
+    git clone https://github.com/qr243vbi/qt6windows7.git
+    cd qt6windows7
+    git checkout aa73dc1aa33989d09e5823532bccb1d31e39bb64
+    SET FGPATCHES=%ROOT_DIR%\\tdesktop\\patches
+    git -c user.email=build@local -c user.name=build am --ignore-whitespace "%FGPATCHES%\\0001-Enabled-DirectComposition-in-Win7-path-and-FLIP_SEQU.patch" "%FGPATCHES%\\0002-Ported-Windows-7-backport-from-Qt-6.10.0-to-Qt-6.11..patch" "%FGPATCHES%\\0003-Added-three-way-port-script-for-moving-overlay-to-ne.patch" "%FGPATCHES%\\0004-Routed-new-6.11-notifyRoleChange-UIA-call-through-wr.patch"
+""")
+
     stage('qt_' + qt, """
     git clone -b v$QT-lts-lgpl https://github.com/qt/qt5.git qt_$QT
     cd qt_$QT
